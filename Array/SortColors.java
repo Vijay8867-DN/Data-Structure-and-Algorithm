2a799a8c068cacff73515f7413f40be16e6e0239
Vijay8867-DN <vijaydn634@gmail.com>
2026-01-21 22:31:20 +0530

    Coding

---
+++ Array/SortColors.java
@@ -0,0 +1,60 @@
+package Array;
+
+import java.util.Arrays;
+
+/*Sort Colors
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+*/
+
+
+public class SortColors {
+
+	public  int[] sortColor(int[] arr) {
+		int n = arr.length;
+		int low = 0, mid = 0, high = n-1;
+		while(mid <= high) {
+			if(arr[mid] == 0) {
+				swap(arr, low, mid);
+				low++;
+				mid++;
+			}else if(arr[mid] == 1) {
+				mid++;
+			}else {
+				swap(arr, mid, high);
+				mid++;
+				high--;
+			}
+		}
+		
+		return arr;
+	}
+	
+	public  int[] swap(int[] arr, int low, int high) {
+		
+			int temp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = temp;
+		
+		return arr;
+	}
+	public static void main(String[] args) {
+		SortColors s = new SortColors();
+		int[] arr = {2,0,2,1,1,0};
+		
+		System.out.println(Arrays.toString(s.sortColor(arr)));
+	
+	}
+}
